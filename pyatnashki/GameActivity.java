54d363394ed0768590a3aee2f2747eb5b59933ac
viktorocean <37244197+viktorocean@users.noreply.github.com>
2018-04-24 16:49:35 +0600

    Update GameActivity.java

--- pyatnashki/GameActivity.java
+++ pyatnashki/GameActivity.java
@@ -1,6 +1,6 @@
 package panoramos.itschool.samsung.com.pyatnashki;
 
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +18,7 @@ public class GameActivity extends MainActivity {
     int[] id = new int[16];
     int[] num = new int[16];
     int[] prov = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
-    int c = 0;
+    //int c = 0;
     int tag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,126 +41,29 @@ public class GameActivity extends MainActivity {
         btn[14] = findViewById(R.id.button15);
         btn[15] = findViewById(R.id.button16);
         Random newRandom = new Random();
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                switch (view.getId())
-                {
-                    case R.id.button:
-                        tag=1;
-                        break; // вызывается для выхода из блока switch (при вызове дальнейший код не исполняется)
-                    case R.id.button2:
-                        tag=2;
-                        break;
-                    case R.id.button3:
-                        tag=3;
-                        break;
-                    case R.id.button4:
-                        tag=4;
-                        break;
-                    case R.id.button5:
-                        tag=5;
-                        break; // вызывается для выхода из блока switch (при вызове дальнейший код не исполняется)
-                    case R.id.button6:
-                        tag=6;
-                        break;
-                    case R.id.button7:
-                        tag=7;
-                        break;
-                    case R.id.button8:
-                        tag=8;
-                        break;
-                    case R.id.button9:
-                        tag=9;
-                        break; // вызывается для выхода из блока switch (при вызове дальнейший код не исполняется)
-                    case R.id.button10:
-                        tag=10;
-                        break;
-                    case R.id.button11:
-                        tag=11;
-                        break;
-                    case R.id.button12:
-                        tag=12;
-                        break;
-                    case R.id.button13:
-                        tag=13;
-                        break; // вызывается для выхода из блока switch (при вызове дальнейший код не исполняется)
-                    case R.id.button14:
-                        tag=14;
-                        break;
-                    case R.id.button15:
-                        tag=15;
-                        break;
-                    case R.id.button16:
-                        tag=16;
-                        break;
+        for (int i = 0; i < 16; ) {
+            int j;
+            int randomNumber = newRandom.nextInt(16);
+            for (j = 0; j < i; j++) {
+                if (num[j] == randomNumber) {
+                    break;
                 }
             }
-        };
-                for (int i = 0; i < 16; ) {
-                    int j;
-                    int randomNumber = newRandom.nextInt(16);
-                    for (j = 0; j < i; j++) {
-                        if (num[j] == randomNumber) {
-                            break;
-                        }
-                    }
-                    if (j == i) {
-                        num[i] = randomNumber;
-                        System.out.print(num[i] + " ");
-                        i++;
-                    }
-                }
-                while (c != 15) {
+            if (j == i) {
+                num[i] = randomNumber;
+                System.out.print(num[i] + " ");
+                i++;
+            }
+        }
+        for (int i = 0; i < 16; i++) {
 
-                    for (int i = 0; i < 16; i++) {
-                        if (num[i] != 0)
-                            btn[i].setText(Integer.toString(num[i]));
-                        else
-                            btn[i].setVisibility(View.INVISIBLE);
-                    }
-// Пробный вариант
-for(int i=0; i<16;i++)
-{
-    btn[i].setOnClickListener(listener);
-}
-                    int n = tag - 1;
-                    //рассчитываем номер строки путем целочисленного деления
-                    int y = n / 4;
-                    //вычисляем столбец
-                    int x = n;
-                    if (n >= 12) x = n - 12;
-                    else if (n >= 8) x = n - 8;
-                    else if (n >= 4) x = n - 4;
-                    //Рассчитываем номер кнопки сверху снизу слева справа и записываем в переменные
-                    //NT NL NR NB NC (Top Left Right Bottom Current)
-                    int nc = y * 4 + x;
-                    int nt = (y - 1) * 4 + x;
-                    int nb = (y + 1) * 4 + x;
-                    int nl = y * 4 + x - 1;
-                    int nr = y * 4 + x + 1;
-                    //Проверяем существование кнопки слева справа снизу сверху
-                    //Если существует то пытаемся поменять кнопки местами
-                    if (y - 1 >= 0)
-                        Swap(btn[nc], btn[nt]);
-                    if (y + 1 < 4)
-                        Swap(btn[nc], btn[nb]);
-                    if (x - 1 >= 0)
-                        Swap(btn[nc], btn[nl]);
-                    if (x + 1 < 4)
-                        Swap(btn[nc], btn[nr]);
+            btn[i].setText(Integer.toString(num[i]));
+            if (num[i] == 0)
+                btn[i].setVisibility(View.INVISIBLE);
+        }
+    }
 
-                    for (int i = 0; i < 15; i++) {
-                        if (Integer.parseInt(btn[i].getText().toString()) == prov[i])
-                            c++;
-                    }
-                }
-        Intent intent = new Intent(this, FinishActivity.class);
-        startActivity(intent);
-//
-
-                }
 
 
             public void Swap(Button B1, Button B2) {
@@ -173,38 +76,48 @@ for(int i=0; i<16;i++)
                 }
             }
 
-           /* public void buttonOnClick(View V) {
-                //Получаем текущую кнопку по которой кликнули
-                Button current = (Button) V;
-                //Получаем номер кнопки из тега кторый задали в XML файле в текстовом режиме редактирования
-                String N = current.getTag().toString();
-                //преобразуем тег из строки в целое число
-                int n = Integer.parseInt(N) - 1;
-                //рассчитываем номер строки путем целочисленного деления
-                int y = n / 4;
-                //вычисляем столбец
-                int x = n;
-                if (n >= 12) x = n - 12;
-                else if (n >= 8) x = n - 8;
-                else if (n >= 4) x = n - 4;
-                //Рассчитываем номер кнопки сверху снизу слева справа и записываем в переменные
-                //NT NL NR NB NC (Top Left Right Bottom Current)
-                int nc = y * 4 + x;
-                int nt = (y - 1) * 4 + x;
-                int nb = (y + 1) * 4 + x;
-                int nl = y * 4 + x - 1;
-                int nr = y * 4 + x + 1;
-                //Проверяем существование кнопки слева справа снизу сверху
-                //Если существует то пытаемся поменять кнопки местами
-                if (y - 1 >= 0)
-                    Swap(btn[nc], btn[nt]);
-                if (y + 1 < 4)
-                    Swap(btn[nc], btn[nb]);
-                if (x - 1 >= 0)
-                    Swap(btn[nc], btn[nl]);
-                if (x + 1 < 4)
-                    Swap(btn[nc], btn[nr]);
-            }*/
-        }
+    public void buttonOnClick(View view) {
+
+        //Получаем текущую кнопку по которой кликнули
+        Button button = (Button) view;
+        //Получаем номер кнопки из тега кторый задали в XML файле в текстовом режиме редактирования
+        //преобразуем тег из строки в целое число
+        int n = Integer.parseInt(button.getTag().toString()) - 1;
+        //рассчитываем номер строки путем целочисленного деления
+        int y = n / 4;
+        //вычисляем столбец
+        int x = n;
+        if (n >= 12) x = n - 12;
+        else if (n >= 8) x = n - 8;
+        else if (n >= 4) x = n - 4;
+        //Рассчитываем номер кнопки сверху снизу слева справа и записываем в переменные
+        //NT NL NR NB NC (Top Left Right Bottom Current)
+        int nc = y * 4 + x;
+        int nt = (y - 1) * 4 + x;
+        int nb = (y + 1) * 4 + x;
+        int nl = y * 4 + x - 1;
+        int nr = y * 4 + x + 1;
+        //Проверяем существование кнопки слева справа снизу сверху
+        //Если существует то пытаемся поменять кнопки местами
+        if (y - 1 >= 0)
+            Swap(btn[nc], btn[nt]);
+        if (y + 1 < 4)
+            Swap(btn[nc], btn[nb]);
+        if (x - 1 >= 0)
+            Swap(btn[nc], btn[nl]);
+        if (x + 1 < 4)
+            Swap(btn[nc], btn[nr]);
+        int c=0;
+            for (int i = 0; i < 15; i++) {
+                if (Integer.parseInt(btn[i].getText().toString()) == prov[i])
+                    c++;
+            }
+
+        if (c == 15) {
+        Intent intent = new Intent(this, FinishActivity.class);
+        startActivity(intent);
+    }
+    }
 
 
+}
